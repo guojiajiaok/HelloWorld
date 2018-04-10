9514d81ef520c8cbfe5bbd4d3c1ff60ca22c2c96
machenike <1097286877@qq.com>
2018-04-10 21:03:47 +0800

    test-primeSum

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World !!!");
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i <= 100; i++) {
+            int j = 2;
+            while (i % j != 0) {
+                j++;
+            }
+            if (i == j) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
